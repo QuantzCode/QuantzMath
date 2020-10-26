830a4b7abe6fed8fe3e227d04c9b6359757b93bc
aayushgenius <69508057+aayushgenius@users.noreply.github.com>
2020-10-26 10:04:10 -0300

    Create forever_fibonnaci_for_any_number.py

---
+++ Algorithms/python/forever_fibonnaci_for_any_number.py
@@ -0,0 +1,9 @@
+#This asks the user for which base number they want. For normal fibonacci use 1
+random = 0
+i = int(input("Starting number?"))
+a = i
+while (random == 0):
+  print(i)
+  print(a)
+  i=a+i
+  a=a+i
